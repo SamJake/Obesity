6545d7259b03ad69b1ce391ff3dd4b7d2692da9c
Sam Jacob <Sam.Jacob@synechron.com>
2016-06-17 17:58:12 +0530

    obesity | redireciton -v2

--- obesity.r
+++ obesity.r
@@ -3,7 +3,11 @@
 
 library(ggplot2)
 library(rvest)
+library(XML)
 
 wiki <- read_html("https://en.wikipedia.org/wiki/Obesity_in_India")
 
-obs <- wiki %>% html_nodes("table") %>% .[[1]]%>% html_table(fill=T)
+#obs <- wiki %>% html_nodes("table") %>% .[[2]]%>% html_table(fill=T)
+
+obs <- html_nodes(wiki,"table")
+obs <- html_table(obs[[2]],fill=T)
