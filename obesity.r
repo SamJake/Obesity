af16273f46e55b552fcc0f7e5f1a2c0b0d7b95b8
Sam Jacob <Sam.Jacob@synechron.com>
2016-06-17 19:42:32 +0530

    obesity | states -v3

--- obesity.r
+++ obesity.r
@@ -1,9 +1,11 @@
 #install.packages("ggplot2")
 #install.packages("rvest")
-
+install.packages("raster")
 library(ggplot2)
 library(rvest)
 library(XML)
+library(raster)
+
 
 wiki <- read_html("https://en.wikipedia.org/wiki/Obesity_in_India")
 
@@ -11,3 +13,15 @@ wiki <- read_html("https://en.wikipedia.org/wiki/Obesity_in_India")
 
 obs <- html_nodes(wiki,"table")
 obs <- html_table(obs[[2]],fill=T)
+str(obs)
+class(obs)
+
+names(obs)
+nrow(obs)
+
+names(obs) <- make.names(names(obs))
+obs
+
+states <- getData("GADM", country="India", level = 1)
+str(states)
+class(states)
